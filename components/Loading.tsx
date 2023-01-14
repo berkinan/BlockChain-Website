5e3a43723cc9313c588d6c99471fa87702a0e511
Umut Önder <umutsonder@gmail.com>
2023-01-14 05:29:04 +0300

    prettier

--- components/Loading.tsx
+++ components/Loading.tsx
@@ -2,19 +2,19 @@ import React from "react";
 import ClipLoader from "react-spinners/ClipLoader";
 
 function Loading() {
-    return (
-        <div className="bg-[#091B18] min-h-screen flex flex-col items-center justify-center text-center">
-        <div className="flex flex-col items-center mb-10">
-            <img
-            className="rounded-full h-56 w-56 mb-10"
-            src="https://i.imgur.com/4h7mAu7.png"
-            alt=""
-            />
-            <h1 className="text-6xl font-bold text-white">Loading</h1>
-        </div>
-        <ClipLoader color='#fff' size={50} />
-        </div>
-        );
+	return (
+		<div className="bg-[#091B18] min-h-screen flex flex-col items-center justify-center text-center">
+			<div className="flex flex-col items-center mb-10">
+				<img
+					className="rounded-full h-56 w-56 mb-10"
+					src="https://i.imgur.com/4h7mAu7.png"
+					alt=""
+				/>
+				<h1 className="text-6xl font-bold text-white">Loading</h1>
+			</div>
+			<ClipLoader color="#fff" size={50} />
+		</div>
+	);
 }
 
 export default Loading;
--- components/Login.tsx
+++ components/Login.tsx
@@ -2,22 +2,27 @@ import { useMetamask } from "@thirdweb-dev/react";
 import React from "react";
 
 function Login() {
-    const connectwithMetamask = useMetamask()
+	const connectwithMetamask = useMetamask();
 
-    return (
-        <div className="bg-[#091B18] min-h-screen flex flex-col items-center justify-center text-center"> 
-            <div className="flex flex-col items-center mb-10">
-            <img className="rounded-full h-56 w-56 mb-10"
-            src = "https://i.imgur.com/4h7mAu7.png"
-            alt = ""/>
-            <h1 className="text-6xl font-bold text-white">Blockchain Lottery</h1>
+	return (
+		<div className="bg-[#091B18] min-h-screen flex flex-col items-center justify-center text-center">
+			<div className="flex flex-col items-center mb-10">
+				<img
+					className="rounded-full h-56 w-56 mb-10"
+					src="https://i.imgur.com/4h7mAu7.png"
+					alt=""
+				/>
+				<h1 className="text-6xl font-bold text-white">Blockchain Lottery</h1>
 
-            <button onClick={connectwithMetamask} className="bg-white px-8 py-5 mt-10 rounded-lg shadow-lg font-bold">
-                Login with Metamask
-            </button>
-            </div>
-        </div>
-    )
+				<button
+					onClick={connectwithMetamask}
+					className="bg-white px-8 py-5 mt-10 rounded-lg shadow-lg font-bold"
+				>
+					Login with Metamask
+				</button>
+			</div>
+		</div>
+	);
 }
 
 export default Login;
