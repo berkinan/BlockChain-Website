29d38e9e14c0d4a09efbd9c0e640d9da0e050294
Umut Önder <umutsonder@gmail.com>
2023-01-23 00:53:53 +0300

    removed logout and auction buttons

--- components/Header.tsx
+++ components/Header.tsx
@@ -8,26 +8,18 @@ function Header() {
 	return (
 		<header className="grid grid-cols-2 md:grid-cols-5 justify-between items-center p-5">
 			<div className="flex items-center space-x-2">
-				<a href="/" className="Header-logo">SuLottery</a>
+				<a href="/" className="Header-logo">
+					SuLottery
+				</a>
 				<div>
-					<h1 className="text-lg text-blue-800 font-bold">Blockchain Lottery</h1>
+					<h1 className="text-lg text-blue-800 font-bold">
+						Blockchain Lottery
+					</h1>
 					<p className="text-sx text-blue-500 truncate">
 						User: {address?.substring(0, 5)}...
 						{address?.substring(address.length, address.length - 4)}
 					</p>
 				</div>
-			</div>
-			<div className="hidden md:flex md:col-span-3 items-center justify-center rounded-md">
-				<div className="bg-[#173A6A] p-4 space-x-2">
-					<NavButton title="Auction" isActive />
-					<NavButton onClick={disconnect} title="Logout" />
-				</div>
-			</div>
-
-			<div className="flex flex-col ml-auto text-right">
-				<span className="md:hidden">
-					<NavButton onClick={disconnect} title="Logout" />
-				</span>
 			</div>
 		</header>
 	);
